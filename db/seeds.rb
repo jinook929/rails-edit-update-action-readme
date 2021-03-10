f55acb0e7f28781a9fbcaddb80ce90c7b031b5fc
jinook929 <jinook929@gmail.com>
2021-03-10 18:04:25 -0500

    commit tree ab96e0cc3716b281ae022f60e8b4bf06b25e3d9e

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Article.create(title: "Title 01", description: "Here is some description for article 01.")
+Article.create(title: "Title 02", description: "Here is some description for article 02.")
+Article.create(title: "Title 03", description: "Here is some description for article 03.")
+Article.create(title: "Title 04", description: "Here is some description for article 04.")
+Article.create(title: "Title 05", description: "Here is some description for article 05.")
